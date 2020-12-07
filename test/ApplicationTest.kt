260c8371df4c4e0402533492d7ece04273f7467d
Tomáš Beňo <benjiko99@gmail.com>
2020-12-07 22:42:41 +0100

    Delete template code

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -6,13 +6,4 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
-    @Test
-    fun testRoot() {
-        withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
-            }
-        }
-    }
 }
